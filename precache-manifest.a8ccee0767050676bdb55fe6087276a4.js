dc45dd95df88e3a70c0fbb915052cdd759335145
Campari229 <KennyKoin@gmail.com>
2020-06-30 20:01:25 +0300

    Updates

---
+++ precache-manifest.a8ccee0767050676bdb55fe6087276a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cdff5f51a5b80a12b487141c0b4d950",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1d664c0a4dfcc0cd8ed7",
+    "url": "./static/css/main.bc8b4447.chunk.css"
+  },
+  {
+    "revision": "6854e665f82c885c8b8c",
+    "url": "./static/js/2.9acb5246.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.9acb5246.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d664c0a4dfcc0cd8ed7",
+    "url": "./static/js/main.26a2d381.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
