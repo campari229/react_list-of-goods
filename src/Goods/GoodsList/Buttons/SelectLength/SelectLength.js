bef3d7f67e7271f80be67b56f5241955481b117b
Campari229 <KennyKoin@gmail.com>
2020-07-02 12:43:53 +0300

    changed onClick to onChange

--- src/Goods/GoodsList/Buttons/SelectLength/SelectLength.js
+++ src/Goods/GoodsList/Buttons/SelectLength/SelectLength.js
@@ -11,7 +11,7 @@ for (let i = 1; i < 11; i += 1) {
 
 export const SelectLength = ({ setMinLength }) => (
   <select
-    onClick={event => setMinLength(event.target.value)}
+    onChange={event => setMinLength(event.target.value)}
     className="select"
   >
     {selectValues.map(value => (
